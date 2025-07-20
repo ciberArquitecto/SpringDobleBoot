815929d8a0400a3756aadbdb587de26f1b2d3083
CiberArquitecto <agente.tecnologo@gmail.com>
2025-07-20 21:51:48 +0200

    Configurar Cors en Spring

--- sql/src/main/java/conSQL/demo/Controladores/Controlador.java
+++ sql/src/main/java/conSQL/demo/Controladores/Controlador.java
@@ -7,6 +7,8 @@ package conSQL.demo.Controladores;
 import conSQL.demo.Entidades.Demo;
 import conSQL.demo.repositorio.DemoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,7 @@ public class Controlador {
         return "Hello World";
     }
     
-    @GetMapping("/guardar")
+    @GetMapping("/guardarObs")
     public String guardar() {
         
         Demo d = new Demo();
@@ -38,6 +40,27 @@ public class Controlador {
         System.out.println(demoRepository.findAll());
         return id.toString();
     }
+
+    @GetMapping("/guardar")
+    public ResponseEntity<String> guardarV2() {        
+        System.out.println("Guardando ...");
+        try {
+            Demo d = new Demo();
+            d.setProp("prop");
+            Long id = demoRepository.save(d).getId();
+            
+            System.out.println(id);
+            System.out.println(demoRepository.findAll().size());
+            
+            return ResponseEntity.ok(id.toString());
+        } catch (Exception e) {
+            System.out.println("Error al guardar");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error en el servidor: " + e.getMessage());
+        }
+    }
+
     
     @GetMapping("/borrarTodo")
     public void borrarTodo() {
